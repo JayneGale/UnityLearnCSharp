b6c27062a063d2fe00dcebf88063685a4afbb642
Jayne Gale <jaynegale@gmail.com>
2020-03-04 23:05:40 +1300

    Delegates and Events

---
+++ UnityLearnCodingCourse/Assets/Scripts/DelegateOnClickEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateOnClickEvent : MonoBehaviour
+{
+    public delegate void ActionOnHitH();
+    public static event ActionOnHitH onHitH;
+
+    public void HitH()
+    {
+        if (onHitH != null)
+        {
+            onHitH();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            onHitH();
+        }
+    }
+
+}
+
---
+++ UnityLearnCodingCourse/Assets/Scripts/TeleportDelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportDelegate : MonoBehaviour
+{
+    void Start()
+    {
+        DelegateOnClickEvent.onHitH += Teleport;
+    }
+
+    private void Teleport()
+    {
+        transform.position = new Vector3(5,2,0);
+        
+    }
+
+    private void OnDisable()
+    {
+        DelegateOnClickEvent.onHitH -= Teleport;
+    }
+}
