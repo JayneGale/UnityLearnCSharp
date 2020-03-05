f9712a40570d48527ef6392dfd3b750a299e9731
Jayne Gale <jaynegale@gmail.com>
2020-03-05 14:16:16 +1300

    LINQ

--- UnityLearnCodingCourse/Assets/Scripts/DelegateOnClickEvent.cs
+++ UnityLearnCodingCourse/Assets/Scripts/DelegateOnClickEvent.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class DelegateOnClickEvent : MonoBehaviour
 {
-    //create a void delegate that calculates the sum of two numbers
+    //create a void delegate (hence Action) that calculates the sum of two numbers
     //use a lambda to avoid having a dedicated method 
+    //now create a void delegate with no parameters (hence Action) that returns gameObject's name
+    //next create a delegate of type int (hence Func not Action) that returns the length of the gamObject's name;
+    //last create a delegate of type int that takes 2 numbers as parameters and adds the sum
 
     public Action<int, int> Sum2Nums;
+    public Action onGetName;
+    public Func<int> getNameLength;
+    public Func<int, int, int> calcSum;
 
     public delegate void ActionOnHitH();
     public static event ActionOnHitH onHitH;
@@ -16,11 +22,20 @@ public class DelegateOnClickEvent : MonoBehaviour
 
     private void Start()
     {
+        onGetName = () => Debug.Log("This GameObject name is " + gameObject.name); // an anonymous method, that has no parameters and no returns
+        onGetName();
+        getNameLength = () => this.gameObject.name.Length;
+        int nameLength = getNameLength();
+        Debug.Log("Name length is " + nameLength);
         Sum2Nums = (a, b) => c = a + b; //one line method via a Lambda expression or can create entire method using {}
-        a = 6;
-        b = 3;
+        a = 12;
+        b = 1;
         Sum2Nums(a, b);
         Debug.Log("Sum of " + a + " and " + b + " = " + c);
+
+        calcSum = (a, b) => a + b;
+        int total = calcSum(9, 3);
+        Debug.Log("Total = " + total);
     }
 
     public void HitH()
@@ -36,6 +51,11 @@ public class DelegateOnClickEvent : MonoBehaviour
         {
             onHitH();
         }
+    }
+
+    int calcSum0(int a, int b)
+    {
+        return a + b;
     }
 
 }
---
+++ UnityLearnCodingCourse/Assets/Scripts/QuizgradesLinq.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuizgradesLinq : MonoBehaviour
+{
+    [System.Serializable]
+    public class Item
+    {
+        public string name;
+        public int itemID;
+        public int buff;
+    }
+    public List<Item> items;
+    public int[] quizGrades = { 0, 52, 76, 84, 88, 46 };
+    public bool hasItem3;
+
+    void Start()
+    {
+        //foreach (var item in items) hasItem3 = item.itemID == 3 ? true: false; 
+        //identify if the list of items has item with itemID3 print true if yes false if not
+        hasItem3 = items.Any(item => item.itemID == 3);
+        string has = hasItem3 ? "True" : "False";
+        Debug.Log("################# Has item with itemID3 = " + has);
+        //grab all the items with buff?20 and print them out
+        var highBuff = items.Where(item => item.buff >= 20);
+        foreach (var item in highBuff)
+            Debug.Log("High buff item " + item.name + " with buff level " + item.buff);
+        //next print the average of all the buff stats
+        var avBuff = items.Average(item => item.buff);
+        Debug.Log("Average of all the buffs is " +avBuff);
+
+        var passingGrades = quizGrades.Where(g => g > 69).OrderByDescending(x => x);
+    }
+
+}
