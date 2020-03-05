d01647823868442cbbbacaf0e0bde51845609eb0
Jayne Gale <jaynegale@gmail.com>
2020-03-05 23:20:12 +1300

    Control patterns replay rewind

---
+++ UnityLearnCodingCourse/Assets/Scripts/Commands/ClickCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+
+public class ClickCommand : ICommands
+{
+    private GameObject _cube;
+    private Color _colour;
+    private Color _previousColour;
+
+    public ClickCommand(GameObject cube, Color colour)
+    {
+        this._cube = cube;
+        this._colour = colour;
+
+    }
+    public void Execute()
+    {
+        _previousColour = _cube.GetComponent<MeshRenderer>().material.color;
+        _cube.GetComponent<MeshRenderer>().material.color = _colour;
+    }
+
+    public void Undo ()
+    {
+        _cube.GetComponent<MeshRenderer>().material.color = _previousColour;
+    }
+
+}
---
+++ UnityLearnCodingCourse/Assets/Scripts/Managers/CommandManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandManager : MonoBehaviour
+{
+    private static CommandManager _instance;
+    public static CommandManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("The Command Manager is null");
+            }
+            return _instance;
+        }
+    }
+
+    private List<ICommands> _commandBuffer = new List<ICommands>();
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // create a method to add commands to the bugger
+    // create a play routing triggerted by a play method to play back alll the commmands
+    //1 second delay
+    //create a rewind routine triggered by a rewind method to play in reverse with 1 second delay
+
+    //done = finished with changing colours, turn them all white
+    // reset method clears the command buffer
+
+
+}
